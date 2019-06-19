e4768107ab5940f35572a9d54ae6815c8cab2f25
Melissa Weinstein <mel.weinstein31@gmail.com>
2019-06-19 12:40:07 +0300

    Cocktails controller

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,32 @@
+class CocktailsController < ApplicationController
+  before_action :set_cocktails, only: [:show]
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(set_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_cocktails
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def set_params
+    params.require(:cocktail).permit(:name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'doses/new'
+  get 'doses/create'
+  get 'doses/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :cocktails, only: [:index, :show, :new, :create]
+
 end
