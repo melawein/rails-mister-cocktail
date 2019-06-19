1a3b4de6e440932714f0d76a2fffb1da5fc7cdcd
Melissa Weinstein <mel.weinstein31@gmail.com>
2019-06-19 20:13:45 +0300

    Update to css on Show page

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -2,7 +2,7 @@ class DosesController < ApplicationController
   def new
     @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new
-    # @ingredient = Ingrxedient.all
+    @ingredient = Ingredient.all
   end
 
   def create
@@ -17,13 +17,14 @@ class DosesController < ApplicationController
 
   def destroy
     @dose = Dose.find(params[:id])
+    cocktail = @dose.cocktail
     @dose.destroy
-    redirect_to cocktail_path(@dose.cocktail)
+    redirect_to cocktail_path(cocktail)
   end
 
   private
 
   def doses_params
-    params.require(:dose).permit(:description, :cocktail_id, :ingredient_id)
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 end
